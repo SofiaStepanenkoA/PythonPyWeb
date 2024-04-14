63c498149c683af6805806c7ec3442835f1aa6ff
murzin.misha2014@yandex.ru <126623679+murzin66@users.noreply.github.com>
2024-04-14 15:07:02 +0300

    corrected queries

--- apps/db_train/views.py
+++ apps/db_train/views.py
@@ -12,18 +12,19 @@ class TrainView(View):
         max_self_esteem = Author.objects.aggregate(max_self_esteem=Max('self_esteem'))
         self.answer1 = Author.objects.filter(self_esteem=max_self_esteem['max_self_esteem'])  # TODO Какие авторы имеют самую высокую уровень самооценки(self_esteem)?
         self.answer2 = Author.objects.annotate(num_entries=Count('entries')).order_by('-num_entries').first()   # TODO Какой автор имеет наибольшее количество опубликованных статей?
-        self.answer3 = Tag.objects.filter(
+        Tags_id= Tag.objects.filter(
             Q(name__icontains='Кино') | Q(name__icontains='Музыка'))  # TODO Какие статьи содержат тег 'Кино' или 'Музыка' ?
+        self.answer3 = Entry.objects.filter(id__in = Tags_id)
         self.answer4 = Author.objects.filter(gender='ж').count()  # TODO Сколько авторов женского пола зарегистрировано в системе?
         self.answer5 = round((Author.objects.filter(status_rule=True).count() / Author.objects.count()) * 100 )   # TODO Какой процент авторов согласился с правилами при регистрации?
 
-        self.answer6 = AuthorProfile.objects.filter(stage__range=(1, 5)).values_list('author__username', flat=True) # TODO Какие авторы имеют стаж от 1 до 5 лет?
-
+        Stage_id = AuthorProfile.objects.filter(stage__range=(1, 5)) # TODO Какие авторы имеют стаж от 1 до 5 лет?
+        self.answer6 = Author.objects.filter(id__in =Stage_id)
         self.answer7 = Author.objects.order_by('-age').first()  # TODO Какой автор имеет наибольший возраст?
         self.answer8 = Author.objects.filter(
             phone_number__isnull=False).count()  # TODO Сколько авторов указали свой номер телефона?
         self.answer9 = Author.objects.filter(age__lt=25)  # TODO Какие авторы имеют возраст младше 25 лет?
-        self.answer10 =Author.objects.annotate(num_entries=Count('entries')).values('username', 'num_entries') # TODO Сколько статей написано каждым автором?
+        self.answer10 =Author.objects.annotate(count=Count('create_at')).values('username', 'count') # TODO Сколько статей написано каждым автором?
 
         context = {f'answer{index}': self.__dict__[f'answer{index}'] for index in range(1, 11)}
 
