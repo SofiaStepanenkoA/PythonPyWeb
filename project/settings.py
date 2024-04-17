3ac9fb0a036f77ab4572509db1c310a0242b7d43
murzin.misha2014@yandex.ru <126623679+murzin66@users.noreply.github.com>
2024-04-17 22:30:49 +0300

    add template.env

--- project/settings.py
+++ project/settings.py
@@ -147,7 +147,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "static/"
 if 'localhost' in ALLOWED_HOSTS:
    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # Папка для локального проекта
 else:
